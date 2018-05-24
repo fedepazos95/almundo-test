b5baaa3570e3226ef3935793c5b26059dfb2de38
Federico Pazos <fede.pazos95@gmail.com>
2018-05-23 22:42:30 -0300

    HotelBlock styled

--- client/src/components/HotelBlock.js
+++ client/src/components/HotelBlock.js
@@ -9,9 +9,9 @@ const HotelBlock = (props) => {
 
             <div className="row">
                 <div className="col-lg-3 col-sm-3">
-                    <img src={image} className="hotel-img" />
+                    <img src={image} className="hotel-img" alt="hotel-img" />
                 </div>
-                <div className="col-lg-5 col-sm-5">
+                <div className="col-lg-6 col-sm-6">
                     <div className="row hotel-title">{props.hotel.name}</div>
                     <div className="row">
                         <Icon name="star" size="20" repeat={props.hotel.stars} />
@@ -26,8 +26,14 @@ const HotelBlock = (props) => {
                         }
                     </div>
                 </div>
-                <div className="col-lg-3 col-sm-3">
-                    3
+                <div className="col-lg-3 col-sm-3 price-box">
+                    <p>
+                        <div>Precio por noche por habitación</div>
+                        <div className="price">
+                            <label>ARS</label> <span>{props.hotel.price}</span>
+                        </div>
+                        <button className="btn-lg btn-primary btn-block">Ver hotel</button>
+                    </p>
                 </div>
             </div>
         </div>
